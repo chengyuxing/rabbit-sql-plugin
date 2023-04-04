869b5929740d1a2590ebe60ba1b8893b03db010d
chengyuxing <chengyuxingo@gmail.com>
2023-04-05 01:22:44 +0800

    next feature: copy sql definition.

---
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/action/CopySqlDefinition.java
@@ -0,0 +1,12 @@
+package com.github.chengyuxing.plugin.rabbit.sql.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class CopySqlDefinition extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
