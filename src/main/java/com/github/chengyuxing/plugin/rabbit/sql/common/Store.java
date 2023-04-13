f0a97c6aaa920b982a79ea39ca06dcc57286ee15
chengyuxing <chengyuxingo@gmail.com>
2023-04-13 10:35:15 +0800

    fixed some bugs

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/common/Store.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/common/Store.java
@@ -1,15 +1,20 @@
 package com.github.chengyuxing.plugin.rabbit.sql.common;
 
+import com.github.chengyuxing.common.io.TypedProperties;
 import com.github.chengyuxing.sql.XQLFileManager;
 import com.github.chengyuxing.sql.exceptions.DuplicateException;
 import com.intellij.openapi.diagnostic.Logger;
 
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public enum Store {
     INSTANCE;
@@ -54,16 +59,6 @@ public enum Store {
         return resourceRoot.resolve(Constants.CONFIG_NAME);
     }
 
-    public void reloadXqlFiles(BiConsumer<Boolean, String> reloaded) {
-        try {
-            xqlFileManager.init();
-            reloaded.accept(true, null);
-        } catch (Exception e) {
-            reloaded.accept(false, e.getMessage());
-            log.warn(e);
-        }
-    }
-
     // TODO: 2023/4/11 if current project is a module, what should i do.
     public void initXqlFiles(BiConsumer<Boolean, String> reloaded) {
         try {
@@ -71,34 +66,36 @@ public enum Store {
             if (resourceRoot != null) {
                 var defaultConfig = xqlConfig();
                 if (defaultConfig != null && Files.exists(defaultConfig)) {
-                    var properties = new Properties();
-                    Map<String, String> files = new HashMap<>();
-                    Set<String> filenames = new HashSet<>();
+                    var properties = new TypedProperties();
                     properties.load(Files.newInputStream(defaultConfig));
-                    properties.forEach((k, s) -> {
-                        String p = k.toString().trim();
-                        String v = s.toString().trim();
-                        if (p.startsWith("files.") && p.length() > 6 && !v.equals("")) {
-                            var alias = p.substring(6);
-                            var abPath = resourceRoot.resolve(v);
-                            if (Files.exists(abPath)) {
-                                files.put(alias, abPath.toUri().toString());
-                            }
-                        }
-                    });
-                    Stream.of(properties.getProperty("filenames", "").split(","))
-                            .map(String::trim)
-                            .filter(name -> !name.equals(""))
+
+                    Map<String, String> files = new HashMap<>();
+                    properties.getMap("files", new HashMap<>())
+                            .forEach((k, v) -> {
+                                var path = v.trim();
+                                if (!path.equals("")) {
+                                    var abPath = resourceRoot.resolve(path);
+                                    if (Files.exists(abPath)) {
+                                        files.put(k, abPath.toUri().toString());
+                                    }
+                                }
+                            });
+
+                    var filenames = properties.getSet("filenames", new HashSet<>())
+                            .stream()
                             .map(resourceRoot::resolve)
                             .filter(Files::exists)
-                            .forEach(p -> filenames.add(p.toUri().toString()));
+                            .map(p -> p.toUri().toString())
+                            .collect(Collectors.toSet());
+
                     xqlFileManager.clearSqlResources();
                     xqlFileManager.clearFiles();
                     xqlFileManager.setFiles(files);
                     xqlFileManager.setFilenames(filenames);
                     xqlFileManager.setCharset(properties.getProperty("charset", "UTF-8"));
                     xqlFileManager.setDelimiter(properties.getProperty("delimiter", ";"));
-                    reloadXqlFiles(reloaded);
+                    xqlFileManager.init();
+                    reloaded.accept(true, null);
                 } else {
                     xqlFileManager.clearSqlResources();
                     xqlFileManager.clearFiles();
@@ -107,9 +104,11 @@ public enum Store {
         } catch (DuplicateException e) {
             reloaded.accept(false, "Warning: " + e.getMessage());
             log.warn(e);
+        } catch (UncheckedIOException e) {
+            log.info("xql file removed!");
         } catch (Exception e) {
             reloaded.accept(false, "Error:" + e.getMessage());
-            log.warn(e);
+            log.error(e);
         }
     }
 }
--- src/test/java/com/github/chengyuxing/plugin/tests/MyCode.java
+++ src/test/java/com/github/chengyuxing/plugin/tests/MyCode.java
@@ -1,12 +1,17 @@
 package com.github.chengyuxing.plugin.tests;
 
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Set;
 
 public class MyCode {
-    public static void main(String[] args) {
-        var set = new java.util.HashSet<>(Arrays.asList("a", "b", "c"));
-        set.remove("a");
+    @Test
+    public void test1() {
+        Path p = Paths.get("file:/Users/chengyuxing/IdeaProjects/rabbit-sql-plugin/src/main/resources/META-INF/plugin.xml");
+        System.out.println(p.getFileName());
     }
 }
