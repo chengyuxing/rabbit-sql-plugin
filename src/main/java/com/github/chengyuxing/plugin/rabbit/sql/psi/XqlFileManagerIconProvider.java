6c129f4f6eed61fa752790dde82bb7d195d40198
chengyuxing <chengyuxingo@gmail.com>
2023-04-16 13:16:59 +0800

    special icon for xql-file-manager.properties

---
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/psi/XqlFileManagerIconProvider.java
@@ -0,0 +1,21 @@
+package com.github.chengyuxing.plugin.rabbit.sql.psi;
+
+import com.github.chengyuxing.plugin.rabbit.sql.common.Constants;
+import com.github.chengyuxing.plugin.rabbit.sql.file.XqlIcons;
+import com.intellij.ide.IconProvider;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class XqlFileManagerIconProvider extends IconProvider {
+    @Override
+    public @Nullable Icon getIcon(@NotNull PsiElement element, int flags) {
+        var psiFile = element.getContainingFile();
+        if (psiFile != null && psiFile.getName().equals(Constants.CONFIG_NAME)) {
+            return XqlIcons.XQL_FILE_MANAGER;
+        }
+        return null;
+    }
+}
