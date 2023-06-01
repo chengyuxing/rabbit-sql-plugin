36e32dc51fa89ffaa536813580eaedd8ddb430cc
chengyuxing <chengyuxingo@gmail.com>
2023-06-01 19:44:49 +0800

    ...

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/IndexFinished.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/IndexFinished.java
@@ -1,12 +1,4 @@
 package com.github.chengyuxing.plugin.rabbit.sql;
 
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.vcs.log.data.index.VcsLogIndex;
-import org.jetbrains.annotations.NotNull;
-
-public class IndexFinished implements VcsLogIndex.IndexingFinishedListener {
-    @Override
-    public void indexingFinished(@NotNull VirtualFile root) {
-
-    }
+public class IndexFinished {
 }
