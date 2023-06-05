a64f801f89f49bb9464c510625e7152c8c623f93
chengyuxing <chengyuxingo@gmail.com>
2023-06-05 13:29:43 +0800

    update dep version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.1.18") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.1.19") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
