c6b1a8559cd760dcc37571e3ffb2ff9c7998f756
chengyuxing <chengyuxingo@gmail.com>
2023-06-21 10:15:43 +0800

    XQL file line marker search scope optimized

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/extensions/GotoJavaCallable.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/extensions/GotoJavaCallable.java
@@ -7,6 +7,7 @@ import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.psi.*;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
@@ -44,49 +45,50 @@ public class GotoJavaCallable extends RelatedItemLineMarkerProvider {
                 if (!Objects.equals(xqlVf.getExtension(), "xql")) {
                     return;
                 }
+                var project = xqlPsiElement.getProject();
+                var module = ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(xqlVf);
+                if (module == null) return;
                 var resource = ResourceCache.getInstance().getResource(xqlFile);
-                if (resource != null) {
-                    var xqlFileManager = resource.getXqlFileManager();
-                    for (Map.Entry<String, String> file : xqlFileManager.getFiles().entrySet()) {
-                        if (file.getValue().equals(xqlVf.toNioPath().toUri().toString())) {
-                            var sqlPath = file.getKey() + "." + sqlName;
-                            if (resource.getXqlFileManager().contains(sqlPath)) {
-                                var project = xqlPsiElement.getProject();
-                                final var sqlRef = "&" + sqlPath;
-                                try {
-                                    List<PsiElement> founded = FilenameIndex.getAllFilesByExt(project, "java", GlobalSearchScope.projectScope(project))
-                                            .stream()
-                                            .filter(vf -> vf != null && vf.isValid())
-                                            .map(vf -> PsiManager.getInstance(project).findFile(vf))
-                                            .filter(Objects::nonNull)
-                                            .map(psi -> {
-                                                final List<PsiElement> psiElements = new ArrayList<>();
-                                                psi.accept(new JavaRecursiveElementWalkingVisitor() {
-                                                    @Override
-                                                    public void visitLiteralExpression(@NotNull PsiLiteralExpression expression) {
-                                                        String v = expression.getValue() instanceof String ? (String) expression.getValue() : null;
-                                                        if (v != null && v.equals(sqlRef)) {
-                                                            psiElements.add(expression);
-                                                        }
-                                                        // unnecessary to do that anymore.
-                                                        // super.visitElement(expression);
+                if (resource == null) return;
+                var xqlFileManager = resource.getXqlFileManager();
+                for (Map.Entry<String, String> file : xqlFileManager.getFiles().entrySet()) {
+                    if (file.getValue().equals(xqlVf.toNioPath().toUri().toString())) {
+                        var sqlPath = file.getKey() + "." + sqlName;
+                        if (resource.getXqlFileManager().contains(sqlPath)) {
+                            final var sqlRef = "&" + sqlPath;
+                            try {
+                                List<PsiElement> founded = FilenameIndex.getAllFilesByExt(project, "java", GlobalSearchScope.moduleScope(module))
+                                        .stream()
+                                        .filter(vf -> vf != null && vf.isValid())
+                                        .map(vf -> PsiManager.getInstance(project).findFile(vf))
+                                        .filter(Objects::nonNull)
+                                        .map(psi -> {
+                                            final List<PsiElement> psiElements = new ArrayList<>();
+                                            psi.accept(new JavaRecursiveElementWalkingVisitor() {
+                                                @Override
+                                                public void visitLiteralExpression(@NotNull PsiLiteralExpression expression) {
+                                                    String v = expression.getValue() instanceof String ? (String) expression.getValue() : null;
+                                                    if (v != null && v.equals(sqlRef)) {
+                                                        psiElements.add(expression);
                                                     }
-                                                });
-                                                return psiElements;
-                                            }).flatMap(Collection::stream)
-                                            .collect(Collectors.toList());
+                                                    // unnecessary to do that anymore.
+                                                    // super.visitElement(expression);
+                                                }
+                                            });
+                                            return psiElements;
+                                        }).flatMap(Collection::stream)
+                                        .collect(Collectors.toList());
 
-                                    if (!founded.isEmpty()) {
-                                        var markInfo = NavigationGutterIconBuilder.create(AllIcons.Actions.DiagramDiff)
-                                                .setTargets(founded)
-                                                .setPopupTitle("Choose reference of sql name \"" + sqlName + "\" (" + founded.size() + " founded)")
-                                                .setTooltipText("Where I am (" + founded.size() + " locations)!")
-                                                .createLineMarkerInfo(xqlPsiElement);
-                                        result.add(markInfo);
-                                    }
-                                } catch (Exception e) {
-                                    log.warn(e);
+                                if (!founded.isEmpty()) {
+                                    var markInfo = NavigationGutterIconBuilder.create(AllIcons.Actions.DiagramDiff)
+                                            .setTargets(founded)
+                                            .setPopupTitle("Choose reference of sql name \"" + sqlName + "\" (" + founded.size() + " founded)")
+                                            .setTooltipText("Where I am (" + founded.size() + " locations)!")
+                                            .createLineMarkerInfo(xqlPsiElement);
+                                    result.add(markInfo);
                                 }
+                            } catch (Exception e) {
+                                log.warn(e);
                             }
                         }
                     }
