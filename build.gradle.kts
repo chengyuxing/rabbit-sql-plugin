a5dc87d9e6d8f955aba94cc3fcf6811fb8d76224
chengyuxing <chengyuxingo@gmail.com>
2024-01-14 00:59:23 +0800

    Sql name quick doc highlight sql optimized.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.8.6") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.8.12") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/util/HtmlUtil.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/util/HtmlUtil.java
@@ -1,19 +1,8 @@
 package com.github.chengyuxing.plugin.rabbit.sql.util;
 
-import com.github.chengyuxing.common.tuple.Pair;
-import com.github.chengyuxing.common.utils.StringUtil;
-import com.github.chengyuxing.sql.Keywords;
-import com.github.chengyuxing.sql.utils.SqlUtil;
-import com.intellij.openapi.diagnostic.Logger;
-
-import java.util.List;
-import java.util.Map;
-
-import static com.github.chengyuxing.common.utils.StringUtil.NEW_LINE;
+import com.github.chengyuxing.sql.utils.SqlHighlighter;
 
 public class HtmlUtil {
-    private static final Logger log = Logger.getInstance(HtmlUtil.class);
-
     public static String toHighlightSqlHtml(String sql) {
         return "<pre>" + highlight(sql) + "</pre>";
     }
@@ -25,66 +14,14 @@ public class HtmlUtil {
     public static String highlight(String sqlString) {
         var sql = sqlString.replace(">", "&gt;")
                 .replace("<", "&lt;");
-        try {
-            Pair<String, Map<String, String>> r = SqlUtil.replaceSqlSubstr(sql);
-            String rSql = r.getItem1();
-            Pair<List<String>, List<String>> x = StringUtil.regexSplit(rSql, "(?<sp>[\\s,\\[\\]():;])", "sp");
-            List<String> maybeKeywords = x.getItem1();
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < maybeKeywords.size(); i++) {
-                String key = maybeKeywords.get(i);
-                if (!key.trim().isEmpty()) {
-                    // keywords highlight
-                    if (rSql.contains(key + "(")) {
-                        maybeKeywords.set(i, colorful(key, Color.FUNCTION));
-                    } else if (StringUtil.equalsAnyIgnoreCase(key, Keywords.STANDARD)) {
-                        maybeKeywords.set(i, colorful(key, Color.KEYWORD));
-                        // functions highlight
-                    } else if (StringUtil.isNumeric(key)) {
-                        maybeKeywords.set(i, colorful(key, Color.NUMBER));
-                        // PostgreSQL function body block highlight
-                    } else if (key.equals("$$")) {
-                        maybeKeywords.set(i, colorful(key, Color.STRING));
-                        // symbol '*' highlight
-                    } else if (key.contains("*") && !key.contains("/*") && !key.contains("*/")) {
-                        maybeKeywords.set(i, key.replace("*", colorful(key, Color.HIGHLIGHT)));
-                    }
-                }
-                sb.append(maybeKeywords.get(i));
-                if (i < maybeKeywords.size() - 1) {
-                    sb.append(x.getItem2().get(i));
-                }
-            }
-            String colorfulSql = sb.toString();
-            // reinsert the sub string
-            Map<String, String> subStr = r.getItem2();
-            for (String key : subStr.keySet()) {
-                colorfulSql = colorfulSql.replace(key, colorful(subStr.get(key), Color.STRING));
-            }
-            // resolve single annotation
-            String[] sqlLine = colorfulSql.split(NEW_LINE);
-            for (int i = 0; i < sqlLine.length; i++) {
-                String line = sqlLine[i];
-                if (line.trim().startsWith("--")) {
-                    sqlLine[i] = colorful(line, Color.ANNOTATION);
-                } else if (line.contains("--")) {
-                    int idx = line.indexOf("--");
-                    sqlLine[i] = line.substring(0, idx) + colorful(line.substring(idx), Color.ANNOTATION);
-                }
-            }
-            colorfulSql = String.join(NEW_LINE, sqlLine);
-            // resolve block annotation
-            if (colorfulSql.contains("/*") && colorfulSql.contains("*/")) {
-                List<String> annotations = SqlUtil.getBlockAnnotation(colorfulSql);
-                for (String annotation : annotations) {
-                    colorfulSql = colorfulSql.replace(annotation, colorful(annotation, Color.ANNOTATION));
-                }
-            }
-            return colorfulSql;
-        } catch (Exception e) {
-            log.warn(e);
-            return sql;
-        }
+        return SqlHighlighter.highlight(sql, (tag, content) -> switch (tag) {
+            case FUNCTION -> colorful(content, Color.FUNCTION);
+            case KEYWORD -> colorful(content, Color.KEYWORD);
+            case NUMBER -> colorful(content, Color.NUMBER);
+            case POSTGRESQL_FUNCTION_BODY_SYMBOL, SINGLE_QUOTE_STRING -> colorful(content, Color.STRING);
+            case ASTERISK -> colorful(content, Color.HIGHLIGHT);
+            case LINE_ANNOTATION, BLOCK_ANNOTATION -> colorful(content, Color.ANNOTATION);
+        });
     }
 
     public static String colorful(String word, Color color) {
