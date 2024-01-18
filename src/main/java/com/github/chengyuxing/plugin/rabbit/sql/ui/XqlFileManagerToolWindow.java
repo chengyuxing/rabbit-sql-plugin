fe3e114276d7d8bec5aec5ead91d5307c7930092
chengyuxing <chengyuxingo@gmail.com>
2024-01-19 02:14:10 +0800

    Plan Add supports for multi xql-file-manager-*.yml.

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/ui/XqlFileManagerToolWindow.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/ui/XqlFileManagerToolWindow.java
@@ -1,19 +1,27 @@
 package com.github.chengyuxing.plugin.rabbit.sql.ui;
 
+import com.github.chengyuxing.plugin.rabbit.sql.file.XqlIcons;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+
 public class XqlFileManagerToolWindow implements ToolWindowFactory, DumbAware {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-
+        toolWindow.getComponent().add(new JButton("Hello world!"));
     }
 
     @Override
-    public boolean isApplicable(@NotNull Project project) {
+    public void init(@NotNull ToolWindow toolWindow) {
+        toolWindow.setIcon(XqlIcons.XQL_FILE_MANAGER);
+    }
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
         return false;
     }
 }
