bc9db7106b566f77ccf9cff6dd9070c438613be7
chengyuxing <chengyuxingo@gmail.com>
2024-01-23 18:13:47 +0800

    tests

--- src/test/java/com/github/chengyuxing/plugin/tests/MyCode.java
+++ src/test/java/com/github/chengyuxing/plugin/tests/MyCode.java
@@ -22,29 +22,30 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class MyCode {
-    @Test
+    //    @Test
     public void test1() {
         Path p = Paths.get(URI.create("file:/Users/chengyuxing/IdeaProjects/rabbit-sql-plugin/src/main/resources/plugin.xml"));
 
         System.out.println(p.endsWith(Path.of("src", "main", "resources", "plugin.xml")));
     }
 
-    @Test
+    //    @Test
     public void test2() throws IOException {
         String sql = Files.readString(Path.of("/Users/chengyuxing/IdeaProjects/rabbit-sql-plugin/src/test/resources/data.sql"));
         StringUtil.getTemplateParameters(sql)
                 .forEach(System.out::println);
     }
 
-    @Test
+    //    @Test
     public void test3() {
         Path path = Path.of("/Users/chengyuxing/IdeaProjects/rabbit-sql-plugin/src/main/resources/plugin.xml");
         System.out.println(path.toUri());
     }
 
-    @Test
+    //    @Test
     public void test5() throws IOException {
         String sql = Files.readString(Path.of("/Users/chengyuxing/IdeaProjects/rabbit-sql-plugin/src/test/resources/data.sql"));
         var keyMapping = StringUtil.getParamsMappingInfo(new SqlGenerator(':'), sql);
@@ -53,24 +54,7 @@ public class MyCode {
         });
     }
 
-    @Test
-    public void test35() {
-        System.out.println(Files.exists(Path.of("")));
-    }
-
-    @Test
-    public void test36() {
-        XQLFileManager xqlFileManager = new XQLFileManager("bbb.yml");
-        System.out.println(xqlFileManager);
-    }
-
-    @Test
-    public void test37() {
-        var sql = new FileResource("data.sql").readString(StandardCharsets.UTF_8);
-//        System.out.println(StringUtil.isVarKeyInForExpression(sql, "user.name"));
-    }
-
-    @Test
+    //    @Test
     public void test38() throws IOException {
         var a = JSON.std.anyFrom("[{\"name\":\"cyx\"},{\"name\":\"jackson\"},{\"name\":\"ikun\"}]");
         var b = JSON.std.anyFrom("{\"id\":1}");
@@ -78,7 +62,7 @@ public class MyCode {
         System.out.println(b);
     }
 
-    @Test
+    //    @Test
     public void testFile() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         var path = Path.of("/Users/chengyuxing/IdeaProjects/sbp-test1")
                 .resolve(Path.of("target", "classes"));
@@ -86,58 +70,26 @@ public class MyCode {
         System.out.println(ReflectUtil.getInstance(clazz));
     }
 
-    @Test
+    //    @Test
     public void testx() throws MalformedURLException, ClassNotFoundException {
-//        URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL("file:/Users/chengyuxing/IdeaProjects/sbp-test1/target/classes/org/example/pipes")});
-//        Class<?> clazz = classLoader.loadClass("org.example.pipes.Big");
-//        System.out.println(clazz);
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL("file:/Users/chengyuxing/IdeaProjects/sbp-test1/target/classes/org/example/pipes")});
+        Class<?> clazz = classLoader.loadClass("org.example.pipes.Big");
+        System.out.println(clazz);
     }
 
-    @Test
+    //    @Test
     public void test23() {
         var user1 = new User("cyx", 23);
         var user2 = new User("cyx", 23);
         System.out.println(user1.hashCode());
         System.out.println(user2.hashCode());
-    }
-
-    @Test
-    public void testXQL() {
-        var xql = new XQLFileManager();
-        System.out.println(xql);
-    }
-
-    @Test
-    public void testSbtApp() throws IOException {
-        var p1 = Pair.of("a", "b");
-        var p2 = Pair.of("a", "b");
-        var a1 = "Hello world!";
-        var a2 = "Hello world!";
-        System.out.println(p1.equals(p2));
-        System.out.println(p1.hashCode() + ":" + p2.hashCode());
-
-        System.out.println(a1 == a2);
-        System.out.println(a1.hashCode() + ":" + a2.hashCode());
-    }
-
-    @Test
-    public void testYield() {
-        System.out.println(ok(10));
-    }
-
-    public boolean ok(int i) {
-        return switch (i) {
-            case 5:
-                yield true;
-            default:
-                yield false;
-        };
+        System.out.println(user1.equals(user2));
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        Class<?> clazz = SimpleJavaCompiler.getInstance().compile("org.example.pipes.Big", Path.of("/Users/chengyuxing/IdeaProjects/sbp-test1/src/main/java/org/example/pipes/Big.java"));
-//        IPipe<?> pipe = (IPipe<?>) clazz.getConstructor().newInstance();
-//        System.out.println(pipe.transform(175));
+        Class<?> clazz = SimpleJavaCompiler.getInstance().compile("org.example.pipes.Big", Path.of("/Users/chengyuxing/IdeaProjects/sbp-test1/src/main/java/org/example/pipes/Big.java"));
+        IPipe<?> pipe = (IPipe<?>) clazz.getConstructor().newInstance();
+        System.out.println(pipe.transform(175));
     }
 
     public record User(String name, int age) {
