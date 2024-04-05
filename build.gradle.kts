28c30d928437aa5cb8ba257ce6e0489082359d8e
chengyuxing <chengyuxingo@gmail.com>
2024-04-05 19:28:36 +0800

    Support rabbit sql 7.8.26+ and rabbit-sql-spring-boot-starter 2.8.22+.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.8.24") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.8.26") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
