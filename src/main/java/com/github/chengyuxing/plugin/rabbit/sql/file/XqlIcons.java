788e2d62f6c6f71a739f1a24f8ef46e9ba0cd42c
chengyuxing <chengyuxingo@gmail.com>
2024-04-06 09:13:29 +0800

    update icons

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/file/XqlIcons.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/file/XqlIcons.java
@@ -9,5 +9,5 @@ public class XqlIcons {
     public static final Icon XQL_ITEM = IconLoader.getIcon("icons/xql-item.svg", XqlIcons.class);
     public static final Icon XQL_FILE_MANAGER = IconLoader.getIcon("icons/xql-file-manager.svg", XqlIcons.class);
     public static final Icon XQL_FILE_MANAGER_SECONDARY = IconLoader.getIcon("icons/xql-file-manager-secondary.svg", XqlIcons.class);
-    public static final Icon XQL_FILE_MANAGER_TOOL_WINDOW = IconLoader.getIcon("icons/xql-file-manager-tool-window.svg", XqlIcons.class);
+    public static final Icon XQL_FILE_MANAGER_TOOL_WINDOW = IconLoader.getIcon("icons/xql-file-manager.svg", XqlIcons.class);
 }
