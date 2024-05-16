4d2416872853562b3c148ac1006f416c0f769ad7
chengyuxing <chengyuxingo@gmail.com>
2024-05-16 12:42:44 +0800

    2.2.11 released

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.chengyuxing"
-version = "2.2.10"
+version = "2.2.11"
 
 repositories {
     mavenLocal()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.8.27") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.8.29") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
