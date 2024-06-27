628ed1121199ed2a575e669461ca245d2c39bf50
chengyuxing <chengyuxingo@gmail.com>
2024-06-27 23:44:14 +0800

    2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.11.0") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.11.1") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
