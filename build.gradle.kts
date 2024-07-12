5f697ce23567c24e3ac38d953a41f4a261b5f559
chengyuxing <chengyuxingo@gmail.com>
2024-07-12 10:58:59 +0800

    2.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.chengyuxing"
-version = "2.4.2"
+version = "2.4.3"
 
 repositories {
     mavenLocal()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.11.3") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.11.4") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
