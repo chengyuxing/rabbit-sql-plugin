45e8ef237b438cc7721fa613b22a6089b2d4f276
chengyuxing <chengyuxingo@gmail.com>
2024-08-20 18:09:34 +0800

    Fixed dynamic sql #for-#done bug.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.chengyuxing"
-version = "2.4.7"
+version = "2.4.8"
 
 repositories {
     mavenLocal()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:7.11.7") {
+    implementation("com.github.chengyuxing:rabbit-sql:7.11.8") {
         exclude("org.slf4j", "slf4j-api")
         exclude("org.yaml", "snakeyaml")
     }
