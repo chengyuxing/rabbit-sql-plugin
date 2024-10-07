58b17c64d58d7fd6a0adaba810161465feff2506
chengyuxing <chengyuxingo@gmail.com>
2024-10-07 19:04:49 +0800

    update

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/Helper.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/Helper.java
@@ -10,7 +10,7 @@ public class Helper extends WebHelpProvider {
     @Override
     public @Nullable String getHelpPageUrl(@NotNull String helpTopicId) {
         return switch (helpTopicId) {
-            case XQL_FILE_MANAGER -> "https://github.com/chengyuxing/rabbit-sql/blob/master/XQL_FILE_MANAGER.md";
+            case XQL_FILE_MANAGER -> "https://github.com/chengyuxing/rabbit-sql?tab=readme-ov-file#xqlfilemanager";
             case XQL_FILE_MANAGER_BAKI_DAO -> "https://github.com/chengyuxing/rabbit-sql?tab=readme-ov-file#autoxfmconfig";
             default -> null;
         };
