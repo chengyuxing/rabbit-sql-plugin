df2dd7acb11b59e3c8c363d74b73bed39bc5afb3
chengyuxing <chengyuxingo@gmail.com>
2024-10-18 23:24:35 +0800

    optimized.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.1.1")
+    version.set("2021.1")
     type.set("IU") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.database",
