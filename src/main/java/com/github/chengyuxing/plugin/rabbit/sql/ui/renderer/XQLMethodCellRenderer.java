85c7763957aaa39309b5bf0a8270d219cce4f650
chengyuxing <chengyuxingo@gmail.com>
2024-10-22 18:07:32 +0800

    App show sql description tooltip.

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/ui/renderer/XQLMethodCellRenderer.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/ui/renderer/XQLMethodCellRenderer.java
@@ -1,13 +1,17 @@
 package com.github.chengyuxing.plugin.rabbit.sql.ui.renderer;
 
+import com.github.chengyuxing.sql.XQLFileManager;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
-public class ColorfulCellRenderer extends DefaultTableCellRenderer {
+public class XQLMethodCellRenderer extends DefaultTableCellRenderer {
+    private final XQLFileManager.Resource resource;
     private final String hexColor;
 
-    public ColorfulCellRenderer(String hexColor) {
+    public XQLMethodCellRenderer(XQLFileManager.Resource resource, String hexColor) {
+        this.resource = resource;
         this.hexColor = hexColor;
     }
 
@@ -15,6 +19,12 @@ public class ColorfulCellRenderer extends DefaultTableCellRenderer {
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         var code = hexColor.substring(1);
         setForeground(new Color(Integer.parseInt(code, 16)));
+        var description = resource.getEntry().get(value.toString()).getDescription();
+        if (column == table.convertColumnIndexToView(0) && !description.isEmpty()) {
+            setToolTipText(description);
+        } else {
+            setToolTipText(null);
+        }
         return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
     }
 }
