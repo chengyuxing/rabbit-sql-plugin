b9fb38c45912daa8b8707151e6f9e674fba88fc2
chengyuxing <chengyuxingo@gmail.com>
2024-10-22 19:39:00 +0800

    color optimized.

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/ui/renderer/XQLMethodCellRenderer.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/ui/renderer/XQLMethodCellRenderer.java
@@ -1,6 +1,7 @@
 package com.github.chengyuxing.plugin.rabbit.sql.ui.renderer;
 
 import com.github.chengyuxing.sql.XQLFileManager;
+import com.intellij.ui.JBColor;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -17,8 +18,9 @@ public class XQLMethodCellRenderer extends DefaultTableCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        var code = hexColor.substring(1);
-        setForeground(new Color(Integer.parseInt(code, 16)));
+        var code = Integer.parseInt(hexColor.substring(1), 16);
+        @SuppressWarnings("UseJBColor") var color = new Color(code);
+        setForeground(new JBColor(color.darker(), color));
         var description = resource.getEntry().get(value.toString()).getDescription();
         if (column == table.convertColumnIndexToView(0) && !description.isEmpty()) {
             setToolTipText(description);
