b63024fb570326520c1c768ef4cc5daaac89095c
chengyuxing <chengyuxingo@gmail.com>
2024-11-13 00:56:39 +0800

    Fixed bugs.

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/actions/toolwindow/popup/GenerateXqlMapperAction.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/actions/toolwindow/popup/GenerateXqlMapperAction.java
@@ -4,8 +4,11 @@ import com.github.chengyuxing.common.tuple.Quadruple;
 import com.github.chengyuxing.plugin.rabbit.sql.common.XQLConfigManager;
 import com.github.chengyuxing.plugin.rabbit.sql.ui.MapperGenerateDialog;
 import com.github.chengyuxing.plugin.rabbit.sql.ui.types.XqlTreeNodeData;
+import com.github.chengyuxing.plugin.rabbit.sql.util.NotificationUtil;
+import com.github.chengyuxing.plugin.rabbit.sql.util.ProjectFileUtil;
 import com.github.chengyuxing.plugin.rabbit.sql.util.SwingUtil;
 import com.intellij.icons.AllIcons;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
@@ -32,7 +35,12 @@ public class GenerateXqlMapperAction extends AnAction {
         if (Objects.nonNull(nodeSource) && nodeSource.getType() == XqlTreeNodeData.Type.XQL_FILE) {
             @SuppressWarnings("unchecked") var data = (Quadruple<String, String, String, XQLConfigManager.Config>) nodeSource.getSource();
             var alias = data.getItem1();
+            var filename = data.getItem3();
             var config = data.getItem4();
+            if (!ProjectFileUtil.isLocalFileUri(filename)) {
+                NotificationUtil.showMessage(project, "remote file not support yet", NotificationType.WARNING);
+                return;
+            }
             ApplicationManager.getApplication().invokeLater(() -> new MapperGenerateDialog(project, alias, config).showAndGet());
         }
     }
