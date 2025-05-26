6cb0d56d4b69a45e5b6f192c3a2031abbb0eca50
chengyuxing <chengyuxingo@gmail.com>
2025-05-26 17:21:23 +0800

    .

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/plugins/database/DatabaseUtil.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/plugins/database/DatabaseUtil.java
@@ -1,5 +1,13 @@
 package com.github.chengyuxing.plugin.rabbit.sql.plugins.database;
 
+import com.intellij.credentialStore.OneTimeString;
+import com.intellij.database.access.DatabaseCredentials;
+import com.intellij.database.access.DbCredentialManager;
+import com.intellij.database.dataSource.DatabaseDriverImpl;
+import com.intellij.database.dataSource.DatabaseDriverManager;
+import com.intellij.database.dataSource.LocalDataSource;
+import com.intellij.database.dataSource.LocalDataSourceManager;
+import com.intellij.database.psi.DbPsiFacade;
 import com.intellij.database.run.ConsoleDataRequest;
 import com.intellij.database.util.DbImplUtil;
 import com.intellij.database.view.ui.DataSourceManagerDialog;
@@ -19,6 +27,16 @@ public class DatabaseUtil {
 
     public static void openDatasourceDialog(Project project) {
         DataSourceManagerDialog.showDialog(project, null, null);
-
+//        LocalDataSource dataSource = new LocalDataSource(true);
+//        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/postgres?currentSchema=test");
+//        dataSource.setUsername("chengyuxing");
+//        DatabaseDriverManager.getInstance().getDrivers().forEach(driver -> {
+//            System.out.println(driver);
+//        });
+//        dataSource.setName("chengyuxingoooooo");
+//        dataSource.setPasswordStorage(LocalDataSource.Storage.PERSIST);
+//        dataSource.setConfiguredByUrl(true);
+//        dataSource.setDatabaseDriver(DatabaseDriverManager.getInstance().getDriver("postgresql"));
+//        LocalDataSourceManager.getInstance(project).addDataSource(dataSource);
     }
 }
