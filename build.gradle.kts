75f84ea9f5aee55db8d326c43c35f629791d1bf5
chengyuxing <chengyuxingo@gmail.com>
2025-11-25 20:29:17 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.chengyuxing"
-version = "2.4.35.231-253"
+version = "2.4.36.231-253"
 
 repositories {
     mavenLocal()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.chengyuxing:rabbit-sql:10.0.3") {
+    implementation("com.github.chengyuxing:rabbit-sql:10.0.5") {
         exclude("org.slf4j", "slf4j-api")
     }
     testImplementation("junit:junit:4.13.2")
