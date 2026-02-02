cac77feab3f6f9c71847c61c35966c16fb4d0a06
chengyuxing <chengyuxingo@gmail.com>
2026-02-02 11:24:59 +0800

    params parse optimized.

---
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/util/RabbitScriptParamParser.java
@@ -0,0 +1,269 @@
+package com.github.chengyuxing.plugin.rabbit.sql.util;
+
+import com.github.chengyuxing.common.script.lang.Token;
+import com.github.chengyuxing.common.script.lang.TokenType;
+import com.github.chengyuxing.common.script.lexer.RabbitScriptLexer;
+import com.github.chengyuxing.common.tuple.Pair;
+import com.github.chengyuxing.common.util.StringUtils;
+import com.github.chengyuxing.sql.util.SqlGenerator;
+import com.github.chengyuxing.sql.util.SqlUtils;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RabbitScriptParamParser {
+    private final SqlGenerator sqlGenerator;
+    private final List<Token> tokens;
+    private int currentTokenIndex;
+    private Token currentToken;
+
+    private final Map<String, Set<String>> paramsMap = new LinkedHashMap<>();
+    private final Set<String> localParams = new HashSet<>();
+    private final List<String> plainSqlParts = new ArrayList<>();
+
+    public RabbitScriptParamParser(String input, SqlGenerator sqlGenerator) {
+        this.sqlGenerator = sqlGenerator;
+        RabbitScriptLexer lexer = new RabbitScriptLexer(input) {
+            @Override
+            protected String normalizeDirectiveLine(String line) {
+                int idx = SqlUtils.indexOfWholeLineComment(line);
+                if (idx != -1) {
+                    return line.substring(idx + 2);
+                }
+                return line;
+            }
+        };
+        this.tokens = lexer.tokenize();
+        this.currentTokenIndex = 0;
+        this.currentToken = tokens.get(currentTokenIndex);
+    }
+
+    private void advance() {
+        currentTokenIndex++;
+        if (currentTokenIndex < tokens.size()) {
+            currentToken = tokens.get(currentTokenIndex);
+        } else {
+            Token lastToken = tokens.get(currentTokenIndex - 1);
+            currentToken = new Token(TokenType.EOF, "", lastToken.getLine(), lastToken.getColumn());
+        }
+    }
+
+    public static Pair<String, String> getKeyAndProp(String name) {
+        String key = name;
+        String prop = "";
+
+        int idx = -1;
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == '.') {
+                idx = i;
+                break;
+            }
+            if (name.charAt(i) == '[') {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx != -1) {
+            key = name.substring(0, idx);
+            prop = name.substring(idx);
+        }
+        return Pair.of(key, prop);
+    }
+
+    private static void scan(
+            String text,
+            Pattern p,
+            BiConsumer<String, Boolean> consumer // true = matched
+    ) {
+        Matcher m = p.matcher(text);
+        int lastEnd = 0;
+        while (m.find()) {
+            if (m.start() > lastEnd) {
+                consumer.accept(text.substring(lastEnd, m.start()), false);
+            }
+            consumer.accept(m.group(), true);
+            lastEnd = m.end();
+        }
+        if (lastEnd < text.length()) {
+            consumer.accept(text.substring(lastEnd), false);
+        }
+    }
+
+    private void appendToken(StringJoiner sb, Token token) {
+        if (token.getType() == TokenType.STRING) {
+            sb.add("'" + token.getValue() + "'");
+            return;
+        }
+        sb.add(token.getValue());
+    }
+
+    private String replaceVarHolder(String var, String content) {
+        Pattern p = Pattern.compile("(?<var>:" + var + ")");
+        StringBuilder sb = new StringBuilder();
+        scan(content, p, (seg, hit) -> {
+            if (hit) {
+                sb.append("_");
+            } else {
+                sb.append(HtmlUtil.code(HtmlUtil.safeEscape(seg), HtmlUtil.Color.ANNOTATION));
+            }
+        });
+        return sb.toString();
+    }
+
+    private void parsePlainText(String text) {
+        sqlGenerator.generatePreparedSql(text, Map.of()).getArgNameIndexMapping()
+                .keySet()
+                .forEach(key -> {
+                    var kp = getKeyAndProp(key);
+                    var var = "_";
+                    var prop = kp.getItem2();
+                    if (!prop.isEmpty()) {
+                        var += HtmlUtil.code(prop, HtmlUtil.Color.ANNOTATION);
+                    }
+                    paramsMap.computeIfAbsent(kp.getItem1(), k -> new LinkedHashSet<>()).add(var);
+                });
+
+        var m = StringUtils.FMT.getPattern().matcher(text);
+        while (m.find()) {
+            var key = m.group("key");
+            if (key.startsWith("!")) {
+                key = key.substring(1);
+            }
+            var kp = getKeyAndProp(key);
+            var var = "_";
+            var prop = kp.getItem2();
+            if (!prop.isEmpty()) {
+                var += HtmlUtil.code(prop, HtmlUtil.Color.ANNOTATION);
+            }
+            var = HtmlUtil.code("${", HtmlUtil.Color.ANNOTATION) + var + HtmlUtil.code("}", HtmlUtil.Color.ANNOTATION);
+            paramsMap.computeIfAbsent(kp.getItem1(), k -> new LinkedHashSet<>()).add(var);
+        }
+    }
+
+    /**
+     * Parse to (key, key expression), e.g. (user, user.addresses[0])
+     *
+     * @return pair
+     */
+    private Pair<String, String> parseExpression() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(currentToken.getValue());
+        advance();
+
+        String keyStart = currentToken.getValue();
+        sb.append(keyStart);
+        advance();
+        while (currentToken.getType() != TokenType.NEWLINE) {
+            if (currentToken.getType() == TokenType.DOT) {
+                sb.append(".");
+                advance();
+                sb.append(currentToken.getValue()); //number
+                advance();
+            } else if (currentToken.getType() == TokenType.LBRACKET) {
+                sb.append("[");
+                advance();
+                sb.append(currentToken.getValue());
+                advance();
+                sb.append("]");
+                advance();
+            } else {
+                break;
+            }
+        }
+        return Pair.of(keyStart, sb.toString());
+    }
+
+    private Set<String> parseExpressions(StringJoiner sb) {
+        Set<String> vars = new LinkedHashSet<>();
+        while (currentToken.getType() != TokenType.NEWLINE) {
+            if (currentToken.getType() == TokenType.COLON) {
+                Pair<String, String> pair = parseExpression();
+                vars.add(pair.getItem1());
+                sb.add(pair.getItem2());
+            } else {
+                appendToken(sb, currentToken);
+                advance();
+            }
+        }
+        return vars;
+    }
+
+    private void parseVarStatement() {
+        StringJoiner sb = new StringJoiner(" ");
+        sb.add(currentToken.getValue()); // #var
+        advance();
+        sb.add(currentToken.getValue()); // var name
+        localParams.add(currentToken.getValue());
+        advance();
+        var vars = parseExpressions(sb);
+        vars.forEach(var -> {
+            String summary = replaceVarHolder(var, sb.toString());
+            paramsMap.computeIfAbsent(var, k -> new LinkedHashSet<>()).add(summary);
+        });
+    }
+
+    private void parseConditionStatement() {
+        StringJoiner sb = new StringJoiner(" ");
+        sb.add(currentToken.getValue());
+        advance();
+        var vars = parseExpressions(sb);
+        vars.forEach(var -> {
+            String summary = replaceVarHolder(var, sb.toString());
+            paramsMap.computeIfAbsent(var, k -> new LinkedHashSet<>()).add(summary);
+        });
+    }
+
+    private void parseForStatement() {
+        StringJoiner sb = new StringJoiner(" ");
+        sb.add(currentToken.getValue()); // #for
+        advance();
+        localParams.add(currentToken.getValue());
+        sb.add(currentToken.getValue());    // item
+        advance();
+        if (currentToken.getType() == TokenType.COMMA) {
+            sb.add(currentToken.getValue());
+            advance();
+            localParams.add(currentToken.getValue());
+            sb.add(currentToken.getValue()); //idx
+            advance();
+        }
+        sb.add(currentToken.getValue()); // of
+        advance();
+        var vars = parseExpressions(sb);
+        vars.forEach(var -> {
+            String summary = replaceVarHolder(var, sb.toString());
+            paramsMap.computeIfAbsent(var, k -> new LinkedHashSet<>()).add(summary);
+        });
+    }
+
+    public void parse() {
+        paramsMap.clear();
+        localParams.clear();
+        plainSqlParts.clear();
+        if (this.tokens.isEmpty()) {
+            return;
+        }
+        while (currentToken.getType() != TokenType.EOF) {
+            switch (currentToken.getType()) {
+                case PLAIN_TEXT -> {
+                    plainSqlParts.add(currentToken.getValue());
+                    advance();
+                }
+                case IF, WHEN, GUARD, SWITCH, CHECK, CASE -> parseConditionStatement();
+                case DEFINE_VAR -> parseVarStatement();
+                case FOR -> parseForStatement();
+                default -> advance();
+            }
+        }
+        String sql = String.join("\n", plainSqlParts);
+        parsePlainText(sql);
+        paramsMap.entrySet().removeIf(entry -> localParams.contains(entry.getKey()));
+    }
+
+    public Map<String, Set<String>> getParamsMap() {
+        return paramsMap;
+    }
+}
--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/util/StringUtil.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/util/StringUtil.java
@@ -1,25 +1,15 @@
 package com.github.chengyuxing.plugin.rabbit.sql.util;
 
-import com.github.chengyuxing.common.script.Token;
-import com.github.chengyuxing.common.script.TokenType;
-import com.github.chengyuxing.common.script.lexer.RabbitScriptLexer;
 import com.github.chengyuxing.common.tuple.Pair;
 import com.github.chengyuxing.common.util.NamingUtils;
-import com.github.chengyuxing.common.util.StringUtils;
 import com.github.chengyuxing.plugin.rabbit.sql.common.XQLConfigManager;
 import com.github.chengyuxing.sql.XQLFileManager;
 import com.github.chengyuxing.sql.util.SqlGenerator;
-import com.github.chengyuxing.sql.util.SqlUtils;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.util.*;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.github.chengyuxing.common.util.StringUtils.NEW_LINE;
-import static com.github.chengyuxing.plugin.rabbit.sql.util.HtmlUtil.code;
 
 public class StringUtil {
     /**
@@ -51,133 +41,9 @@ public class StringUtil {
     }
 
     public static Map<String, Set<String>> getParamsMappingInfo(SqlGenerator sqlGenerator, String sql) {
-        Map<String, Set<String>> paramsMap = new LinkedHashMap<>();
-        RabbitScriptLexer lexer = new RabbitScriptLexer(sql) {
-            @Override
-            protected String normalizeDirectiveLine(String line) {
-                int idx = SqlUtils.indexOfWholeLineComment(line);
-                if (idx != -1) {
-                    return line.substring(idx + 2);
-                }
-                return line;
-            }
-        };
-        var tokens = lexer.tokenize();
-        var sqlTokens = new ArrayList<Token>();
-        var scriptTokens = new ArrayList<Token>();
-        for (var token : tokens) {
-            if (token.getType() == TokenType.PLAIN_TEXT) {
-                sqlTokens.add(token);
-            } else {
-                scriptTokens.add(token);
-            }
-        }
-
-        var localParams = new HashSet<String>();
-        for (int i = 0, j = scriptTokens.size(); i < j; i++) {
-            var token = scriptTokens.get(i);
-            if (token.getType() == TokenType.IF ||
-                    token.getType() == TokenType.SWITCH ||
-                    token.getType() == TokenType.WHEN ||
-                    token.getType() == TokenType.FOR ||
-                    token.getType() == TokenType.CHECK ||
-                    token.getType() == TokenType.GUARD ||
-                    token.getType() == TokenType.DEFINE_VAR) {
-                List<Token> scripts = new ArrayList<>();
-                List<Token> vars = new ArrayList<>();
-                while (scriptTokens.get(i).getType() != TokenType.NEWLINE) {
-                    var itemToken = scriptTokens.get(i);
-                    if (itemToken.getType() == TokenType.VARIABLE_NAME) {
-                        vars.add(itemToken);
-                    } else if (itemToken.getType() == TokenType.FOR) {
-                        // #for item,idx of :list
-                        if (i + 1 < j && scriptTokens.get(i + 1).getType() == TokenType.IDENTIFIER) {
-                            localParams.add(scriptTokens.get(i + 1).getValue());
-                        }
-                        if (i + 3 < j && scriptTokens.get(i + 3).getType() == TokenType.IDENTIFIER) {
-                            localParams.add(scriptTokens.get(i + 3).getValue());
-                        }
-                    } else if (itemToken.getType() == TokenType.DEFINE_VAR) {
-                        // #var id =
-                        if (i + 1 < j && scriptTokens.get(i + 1).getType() == TokenType.IDENTIFIER) {
-                            localParams.add(scriptTokens.get(i + 1).getValue());
-                        }
-                    }
-                    scripts.add(itemToken);
-                    i++;
-                }
-                for (Token var : vars) {
-                    String name = var.getValue().substring(1);
-                    var kp = getKeyAndProp(name);
-                    var varKey = kp.getItem1();
-                    if (!paramsMap.containsKey(varKey)) {
-                        paramsMap.put(varKey, new LinkedHashSet<>());
-                    }
-                    var ss = scripts.stream().map(t -> {
-                        if (t.getType() == TokenType.VARIABLE_NAME) {
-                            var tkp = getKeyAndProp(t.getValue().substring(1));
-                            if (tkp.getItem1().equals(varKey)) {
-                                if (tkp.getItem2().isEmpty()) {
-                                    return "_";
-                                }
-                                return "_" + code(tkp.getItem2(), HtmlUtil.Color.ANNOTATION);
-                            }
-                        }
-                        return code(HtmlUtil.safeEscape(t.getValue()), HtmlUtil.Color.ANNOTATION);
-                    }).collect(Collectors.joining(" "));
-                    paramsMap.get(varKey).add(ss);
-                }
-            }
-        }
-
-        var plainSql = sqlTokens.stream()
-                .map(Token::getValue)
-                .collect(Collectors.joining(NEW_LINE));
-        sqlGenerator.generatePreparedSql(plainSql, Map.of()).getArgNameIndexMapping()
-                .keySet()
-                .forEach(k -> {
-                    var kp = getKeyAndProp(k);
-                    if (!paramsMap.containsKey(kp.getItem1())) {
-                        paramsMap.put(kp.getItem1(), new LinkedHashSet<>());
-                    }
-                    var prop = kp.getItem2();
-                    var holder = "_";
-                    if (!prop.isEmpty()) {
-                        holder = holder + code(prop, HtmlUtil.Color.ANNOTATION);
-                    }
-                    paramsMap.get(kp.getItem1()).add(holder);
-                });
-
-        var tempP = StringUtils.FMT.getPattern();
-        var tempM = tempP.matcher(plainSql);
-        while (tempM.find()) {
-            var key = tempM.group("key");
-            if (key.startsWith("!")) {
-                key = key.substring(1);
-            }
-            if (key.contains(".")) {
-                key = key.substring(0, key.indexOf("."));
-            }
-            if (!paramsMap.containsKey(key)) {
-                paramsMap.put(key, new LinkedHashSet<>());
-            }
-            var temp = tempM.group(0).replace(key, "*");
-            var coloredTemp = code(temp.substring(0, temp.indexOf("*")), HtmlUtil.Color.ANNOTATION) + code("_", HtmlUtil.Color.LIGHT) + code(temp.substring(temp.indexOf("*") + 1), HtmlUtil.Color.ANNOTATION);
-            paramsMap.get(key).add(coloredTemp);
-        }
-        paramsMap.entrySet().removeIf(entry -> localParams.contains(entry.getKey()));
-        return paramsMap;
-    }
-
-    public static Pair<String, String> getKeyAndProp(String name) {
-        String key = name;
-        String prop = "";
-        int dotIdx = name.indexOf(".");
-        if (dotIdx != -1) {
-            key = name.substring(0, dotIdx);
-            prop = name.substring(dotIdx);
-        }
-        return Pair.of(key, prop);
+        var parser = new RabbitScriptParamParser(sql, sqlGenerator);
+        parser.parse();
+        return parser.getParamsMap();
     }
 
     public static String camelizeAndClean(String content) {
