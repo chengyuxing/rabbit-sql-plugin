f5292b6e169263d7adf593c50657b61964ad4d9a
chengyuxing <chengyuxingo@gmail.com>
2026-07-01 21:45:26 +0800

    2.4.56

--- src/main/java/com/github/chengyuxing/plugin/rabbit/sql/Helper.java
+++ src/main/java/com/github/chengyuxing/plugin/rabbit/sql/Helper.java
@@ -8,12 +8,14 @@ public class Helper extends WebHelpProvider {
     public static final String XQL_FILE_MANAGER = "com.github.chengyuxing.rabbit-sql-plugin.xql-file-manager";
     public static final String XQL_FILE_MANAGER_BAKI_DAO = "com.github.chengyuxing.rabbit-sql-plugin.xql-file-manager.baki-dao";
     public static final String SPRING_INTERFACE_MAPPER_USAGE = "com.github.chengyuxing.rabbit-sql-plugin.xql-mapper-usage";
+    public static final String DOMAIN = "domain";
     @Override
     public @Nullable String getHelpPageUrl(@NotNull String helpTopicId) {
         return switch (helpTopicId) {
             case XQL_FILE_MANAGER -> "https://github.com/chengyuxing/rabbit-sql?tab=readme-ov-file#xqlfilemanager";
             case XQL_FILE_MANAGER_BAKI_DAO -> "https://github.com/chengyuxing/rabbit-sql?tab=readme-ov-file#autoxfmconfig";
             case SPRING_INTERFACE_MAPPER_USAGE -> "https://github.com/chengyuxing/rabbit-sql-spring-boot-starter?tab=readme-ov-file#simple-usage";
+            case DOMAIN -> "https://rabbit-sql.com";
             default -> null;
         };
     }
